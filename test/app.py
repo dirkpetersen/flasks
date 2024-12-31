4143f5a982de8cc9877c91724fd9d9c7ebb745d0
Dirk Petersen <dirk@somewhere>
2024-12-30 16:05:28 -0800

    refactor: Make Redis database configurable via environment variable

--- test/app.py
+++ test/app.py
@@ -14,7 +14,7 @@ load_dotenv()
 redis_client = redis.Redis(
     host=os.getenv('REDIS_HOST', 'localhost'),
     port=int(os.getenv('REDIS_PORT', 6379)),
-    db=0,
+    db=int(os.getenv('REDIS_DB', 0)),
     decode_responses=True
 )
 
