112379a92075a70b8102d22dc8ef24b65b343636
Dirk Petersen <dirk@somewhere>
2025-01-05 18:09:08 -0800

    disable ses verification

--- test4/email_verification.py
+++ test4/email_verification.py
@@ -50,12 +50,12 @@ def send_verification_email(email: str, token: str) -> bool:
     
     try:
         # First verify the email address if not already verified
-        try:
-            ses.get_identity_verification_attributes(Identities=[email])['VerificationAttributes'][email]
-        except (KeyError, ClientError):
-            # Email not verified, send verification request
-            ses.verify_email_identity(EmailAddress=email)
-            return True  # Return true but don't send verification email yet
+        # try:
+        #     ses.get_identity_verification_attributes(Identities=[email])['VerificationAttributes'][email]
+        # except (KeyError, ClientError):
+        #     # Email not verified, send verification request
+        #     ses.verify_email_identity(EmailAddress=email)
+        #     return True  # Return true but don't send verification email yet
             
         # If we get here, email is verified, send the verification email
         response = ses.send_email(
